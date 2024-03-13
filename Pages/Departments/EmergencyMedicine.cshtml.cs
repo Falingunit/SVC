327089a8036d3592d7d70f99a1f82f9f9d402fb6
Falingunit <84975935+Falingunit@users.noreply.github.com>
2024-03-13 13:23:38 +0530

    Add project files.

---
+++ Pages/Departments/EmergencyMedicine.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SVC.Pages.Departments
+{
+    public class EmergencyMedicineModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
