f507d32fbd0bd3d81665cde42abf42ea67df818b
sbani <sbani@FALINGUNIT>
2024-06-27 14:17:58 +0530

    v 1.0

--- Models/VacantPosition.cs
+++ Models/VacantPosition.cs
@@ -1,12 +1,18 @@
-﻿using System.Text.Json;
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using System.Text.Json;
 
 namespace SVC.Models
 {
     public class VacantPosition
     {
         public string Title { get; set; }
+        public string Deadline {  get; set; }
+        public IEnumerable<string> Education { get; set; }
+        public string Shift { get; set; }
+        public IEnumerable<string> Experience { get; set; }
+        public IEnumerable<string> Skills { get; set; }
+        public IEnumerable<string> Responsibilites { get; set; }
         public string Description { get; set; }
-        public string ExpectedSalary { get; set; }
         public string Forms { get; set; }
     }
 }
