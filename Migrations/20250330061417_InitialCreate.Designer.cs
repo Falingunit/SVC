5a94e29f7905fbdf09bdf8e4390409bda5a2d3a6
sbani <sbani@FALINGUNIT>
2025-03-30 11:49:06 +0530

    Fixed up som stuff and updated

--- Migrations/20250330061417_InitialCreate.Designer.cs
+++ Migrations/20250330061417_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SVC.DatabaseContexts;
 
@@ -11,9 +12,11 @@ using SVC.DatabaseContexts;
 namespace SVC.Migrations
 {
     [DbContext(typeof(SVCDatabaseContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20250330061417_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
