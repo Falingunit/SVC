bdc2a6002ad25356a4da5fc40da37f28772784f5
Falingunit <84975935+Falingunit@users.noreply.github.com>
2025-04-15 23:28:36 +0530

    Added feedback menu.

---
+++ Pages/Feedback.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SVC.Models;
+using SVC.Services;
+
+namespace SVC.Pages
+{
+    public class FeedbackModel : PageModel
+    {
+    }
+}
