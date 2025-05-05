aca75a6264da60675fa6ba11380f403e358e07ec
sbani <sbani@FALINGUNIT>
2025-05-05 23:17:00 +0530

    Changed copyright and contact us to contact information

--- Program.cs
+++ Program.cs
@@ -11,13 +11,6 @@ builder.Services.AddTransient<JsonFileVacantPositionService>();
 builder.Services.AddDbContext<SVCDatabaseContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version(8, 0, 21))));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
